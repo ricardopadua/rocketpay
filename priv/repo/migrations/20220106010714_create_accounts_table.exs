75da921547bcc529d7de5a7909213bf857c141ca
Ricardo Pádua <ricardo.padua4@gmail.com>
2022-01-05 22:51:44 -0300

    feat(account) add migration accounts

---
+++ priv/repo/migrations/20220106010714_create_accounts_table.exs
@@ -0,0 +1,17 @@
+defmodule Rocketpay.Repo.Migrations.CreateAccountsTable do
+  use Ecto.Migration
+
+  def up do
+    create table :accounts do
+      add :balance, :decimal
+      add :user_id, references(:users, type: :binary_id)
+
+      timestamps()
+    end
+    create constraint(:accounts, :balance_must_be_positive_or_zero, check: "balance >= 0")
+  end
+
+  def down do
+    drop table :accounts
+  end
+end
