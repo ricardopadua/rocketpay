3e3f61e1408e8c913323082a343c7ddb183c5dbc
Ricardo Pádua <ricardo.padua4@gmail.com>
2022-01-12 08:22:00 -0300

    create transaction create users

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :rocketpay, Rocketpay.Repo,
   username: "postgres",
-  password: "leruaite",
+  password: "postgres",
   database: "rocketpay_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
